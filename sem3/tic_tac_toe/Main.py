fbaa26187b627fac571adb66fa3bf99781e27da3
kate <01redcat@mail.ru>
2023-11-22 13:29:07 +0300

    крестики нолики ООП

--- sem3/tic_tac_toe/Main.py
+++ sem3/tic_tac_toe/Main.py
@@ -5,18 +5,20 @@ game = Game()
 view = View()
 game.dash.print_maps()
 
-while game.game_over == False: # назначаем условие выхода из цикла
-    step_x = view.get_step(game.player) # получаем ход игрока 1
-    game.player = '2' # передаем ход игроку 2
-    step_0 = view.get_step(game.player)# получаем ход игрока 2
-    game.player = '1'# передаем ход игроку 1
-    game.step(step_x, game.symbol_x)# добавляем в карту ход игрока 1
-    game.step(step_0, game.symbol_0)# добавляем в карту ход игрока 2
-    game.dash.print_maps()# отрисовываем карту с учетом последних ходов
-    win = game.win()# проверяем условие выхода из цикла
+while game.game_over == False:  # назначаем условие выхода из цикла
+    step_x = view.get_step(game.player)  # получаем ход игрока 1
+    game.player = '2'  # передаем ход игроку 2
+    step_0 = view.get_step(game.player)  # получаем ход игрока 2
+    game.player = '1'  # передаем ход игроку 1
+    game.step(step_x, game.symbol_x)  # добавляем в карту ход игрока 1
+    game.step(step_0, game.symbol_0)  # добавляем в карту ход игрока 2
+    game.dash.print_maps()  # отрисовываем карту с учетом последних ходов
+    win = game.win()  # проверяем условие выхода из цикла
     if win != "":
         game.game_over = True
     else:
         game.game_over = False
-    
+
 print("Победил", win)
+
+# Использовано: ООП, структурное программирование, декларативное (готовый метод index() в функции step() class Game), императивное
